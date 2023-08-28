1e850f0aa5e9ca7f44e4cdc902071d6b07ccd7e7
BenjaminHGong <94771541+BenjaminHGong@users.noreply.github.com>
2023-08-27 18:35:33 -0700

    Update setup.py

--- bot/setup.py
+++ bot/setup.py
@@ -1,15 +1,22 @@
 import json
+import os
 
 data = {
     "autorespond": {},
     "bank": {},
     "botwhitelist": [],
-    "channelwhitelist":[],
+    "channelwhitelist": [],
     "grammar": [],
     "mute": [],
     "reminders": {}
 }
 
 for filename, content in data.items():
-    with open(f"data/{filename}.json", "w") as file:
-        json.dump(content, file, indent=4)
+    file_path = f"data/{filename}.json"
+
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            json.dump(content, file, indent=4)
+            print(f"Created {filename}.json")
+    else:
+        print(f"{filename}.json already exists")
